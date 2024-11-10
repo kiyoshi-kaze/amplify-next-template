a7992accf1832cfaf61624ef5472e846ac2bc39c
kiyoshi-kaze <kaze@kokontouzai.jp>
2024-11-10 21:04:15 +0900

    added menue

--- app/setting/layout.tsx
+++ app/setting/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./app.css";
+//import "./globals.css";
+import Header from "./../components/header"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header/>
+        <Header/>
         {children}
       </body>
     </html>
