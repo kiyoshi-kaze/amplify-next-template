d6fe47f564b8a0a6a087e1f0532532f5b7d48a85
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-01-21 23:19:27 +0900

    Device-query

--- amplify/data/getPost.js
+++ amplify/data/getPost.js
@@ -1,7 +1,23 @@
 //step4にて追加。
-import * as ddb from "@aws-appsync/utils/dynamodb";
+//import * as ddb from "@aws-appsync/utils/dynamodb";
+
+//export function request(ctx) {
+  //return ddb.get({ key: { Device: ctx.args.Device } });
+//}
+//export const response = (ctx) => ctx.result;
+
+
+
+import { util } from '@aws-appsync/utils';
 
 export function request(ctx) {
-  return ddb.get({ key: { Device: ctx.args.Device } });
+  const { owner } = ctx.args;
+  return {
+    operation: 'Query',
+    query: {
+      expression: 'ownerId = :ownerId',
+      expressionValues: util.dynamodb.toMapValues({ ':ownerId': owner })
+    },
+    index: 'Controller-index'
+  };
 }
-export const response = (ctx) => ctx.result;
