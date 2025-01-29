185ee44bac6fde81f91626f08b04971e0069a37d
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-01-29 23:31:48 +0900

    Iot

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -85,10 +85,7 @@ const schema = a.schema({
     .handler(
       a.handler.custom({
         dataSource: "ExternalPostTableDataSource",
-        //entry: "./listDeviceByController.js",
-        //entry: "./listDeviceByControllerType.js",
-        entry: "./listIotDataByController.js",
-
+        entry: "./listIot.js",
       })
     ),
 
