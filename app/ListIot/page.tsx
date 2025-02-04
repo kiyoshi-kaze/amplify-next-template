8b3c5b6387f81bd6e29225826124bbb94e53b0d2
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-02-04 23:19:47 +0900

    menu3

--- app/ListIot/page.tsx
+++ app/ListIot/page.tsx
@@ -40,17 +40,8 @@ export default function App() {
   }
 
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
   useEffect(() => {
-    listTodos();
-    getPost(); // Postの初期表示
     listIot (); // Postの初期表示
-    listIotDataByController (); // Postの初期表示
 
     //サブスクリプションの設定をuseEffect()の中に移動。
     const sub = client.subscriptions.receivePost()
@@ -67,40 +58,9 @@ export default function App() {
   //}, []);
   }, [startDate, endDate]);//★startDatetimeとendDatetimeが変更されたときにlistIot関数を呼び出す
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
-  //step5にて追加。
-  async function addPost () {
-    const {data} = await client.mutations.addPost({
-      Controller: window.prompt("Controller"),
-
-    },{authMode: "apiKey"});
-    //console.log(data)
-  }
-
-  //getPostを追記
-  async function getPost () {
-
-    const { data, errors } = await client.queries.getPost({
-      Device: "AC233FA3DA16" ,//任意のDeviceをキーに1件抽出。
-    });
-    console.log('get=',data)
-
-    //画面への転送を追記
-    if (data) {
-      setPosts(prevPosts => [...prevPosts, data]);
-    }
-  }
-
   //Iotのデータを抽出。
     async function listIot () {
 
-      //const startDatetime = `${startDate} 00:00:00+09:00`;
-      //const endDatetime = `${endDate} 23:59:59+09:00`;
       const startDatetime = `${format(startDate, "yyyy-MM-dd")} 00:00:00+09:00`;
       const endDatetime = `${format(endDate, "yyyy-MM-dd")} 23:59:59+09:00`;
 
@@ -110,77 +70,16 @@ export default function App() {
       const { data, errors } = await client.queries.listIot({
 
         Controller: "Mutsu01",//Controllerが"Mutsu01"であるデータを抽出。
-        //DeviceDatetime: "2024-06-30 23:28:28+09:00",
-        //StartDatetime: "2025-01-31 00:00:00+09:00",//範囲検索
         StartDatetime: startDatetime,//★修正
-        //EndDatetime: "2025-01-31 23:59:59+09:00",//範囲検索
         EndDatetime: endDatetime,//★修正
       });
       console.log('listIot=',data)
   
     }
 
-  //listIotByControllerを追記。
-  async function listIotDataByController () {
-
-
-
-    console.log('page called'); // 関数が呼び出されたことを確認
-    try {  
-      const { data, errors } = await client.queries.listIotDataByController({
-        Controller: "Mutsu01",//Controllerが"Mutsu01"であるデータを抽出。
-        DeviceDatetime: "2024-06-30 23:28:28+09:00",
-      });
-    
-      if (errors) {
-        console.error('Query エラー', errors); // エラーがある場合にログ出力
-      } else if (data) {
-        console.log('Query 結果', data); // クエリ結果をログ出力
-      } else {
-        console.log('データ無し'); // データが返されなかった場合
-      }
-
-    } catch (error) {
-      console.error('予期しないエラー', error); // 予期しないエラーをログ出力
-    }
-  }
-
-
-  // リストボックスコンポーネントを追加
-  //function handleStartDatetimeChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    //setStartDatetime(event.target.value);
-  //}
-
-  //function handleEndDatetimeChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    //setEndDatetime(event.target.value);
-  //}
-
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-
-      <h1>My posts</h1>
-      <button onClick={addPost}>+ new post</button>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.Device}>{post.Controller}</li>
-        ))}
-      </ul>
-
-      <h1>My lists</h1>
-      <button onClick={addPost}>+ new post</button>
-      <ul>
-        {devices.map((device) => (
-          <li key={device.Device}>{device.Controller}</li>
-        ))}
-      </ul>
 
       <div>
         <label>
@@ -193,13 +92,6 @@ export default function App() {
         </label>
       </div>
 
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
     </main>
   );
 }
