62ef3e154017a396e069e9be6de8b3241acb3b98
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-02-04 23:07:50 +0900

    menu3

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <main>
       <h1>トップ画面</h1>
-      <ul>
-        <li><Link href="/ListIot">ListIotページ</Link></li>
-        <li><Link href="/TableDevice">TableDeviceページ</Link></li>
-      </ul>
     </main>
   );
 }
