0bdc5527877ac73090d3bb3dd624fb32baa8ccba
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-02-12 21:31:22 +0900

    dot

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -1,2 +1,9 @@
-import { createMap } from 'maplibre-gl-js-amplify';
-import 'maplibre-gl/dist/maplibre-gl.css';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>トップ画面</h1>
+    </main>
+  );
+}
