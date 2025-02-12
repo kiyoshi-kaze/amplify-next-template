489782b1f56a3ca3c528a26e686d37837c5a31cd
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-02-12 22:20:28 +0900

    map

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -3,10 +3,31 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 
 import Link from 'next/link';
 
+
+
+import { useEffect } from 'react';
+import maplibregl from 'maplibre-gl';
+
 export default function Home() {
+  useEffect(() => {
+    async function initializeMap() {
+      const map = new maplibregl.Map({
+        container: 'map', // An HTML Element or HTML element ID to render the map in
+        style: 'https://demotiles.maplibre.org/style.json', // Your map style URL
+        center: [-123.1187, 49.2819], // [Longitude, Latitude]
+        zoom: 11,
+      });
+    }
+    initializeMap();
+  }, []);
+
   return (
-    <main>
-      <h1>地図画面</h1>
-    </main>
+    <div>
+      <h1>Map Example</h1>
+      <div id="map" style={{ width: '100%', height: '500px' }}></div>
+    </div>
   );
 }
+
+
+
