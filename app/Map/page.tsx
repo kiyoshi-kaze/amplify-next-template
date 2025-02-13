706238e7d0c0835cc7e27b5e7b383c50fa554f57
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-02-13 22:41:04 +0900

    map

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, useState, useCallback } from "react";
+import { FC, useState, useCallback, useEffect } from "react";
 import * as maplibregl from "maplibre-gl";
 import Map, { ViewState } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
@@ -13,12 +13,12 @@ const InitialViewState: Omit<ExtendedViewState, 'width' | 'height'> = {
   longitude: 140.303551,
   latitude: 35.352952,
   zoom: 15,
-  pitch: 45, // マップの初期ピッチ (傾き)
-  bearing: 0, // マップの初期ベアリング (回転)
-  padding: { top: 0, bottom: 0, left: 0, right: 0 } // 必要な padding プロパティを追加
+  pitch: 45,
+  bearing: 0,
+  padding: { top: 0, bottom: 0, left: 0, right: 0 }
 };
 
-const MAX_PITCH = 85 as const; // マップの最大ピッチ角度
+const MAX_PITCH = 85 as const;
 const MAX_ZOOM = 15 as const;
 const MIN_ZOOM = 1 as const;
 
@@ -30,17 +30,56 @@ const TerrainMap: FC = () => {
   });
 
   const handleMoveEnd = useCallback((event: { viewState: ViewState }) => {
-    setViewState((prev) => ({ ...prev, ...event.viewState }));
+    setViewState((prev) => ({
+      ...prev,
+      ...event.viewState,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }));
+  }, []);
+
+  useEffect(() => {
+    const map = new maplibregl.Map({
+      container: 'map',
+      style: "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json",
+      center: [InitialViewState.longitude, InitialViewState.latitude],
+      zoom: InitialViewState.zoom,
+      pitch: InitialViewState.pitch,
+      bearing: InitialViewState.bearing
+    });
+
+    // ナビゲーションコントロールを追加
+    const nav = new maplibregl.NavigationControl({});
+    map.addControl(nav, 'top-left');
+
+    map.on('moveend', () => {
+      const newViewState = {
+        longitude: map.getCenter().lng,
+        latitude: map.getCenter().lat,
+        zoom: map.getZoom(),
+        pitch: map.getPitch(),
+        bearing: map.getBearing(),
+      };
+      setViewState((prev) => ({
+        ...prev,
+        ...newViewState,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }));
+    });
+
+    return () => {
+      map.remove();
+    };
   }, []);
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div id="map" style={{ width: "100vw", height: "100vh" }}>
       <Map
         mapLib={maplibregl}
         mapStyle="https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json"
         initialViewState={InitialViewState}
         viewState={viewState}
-        onMoveEnd={(event) => handleMoveEnd(event)}
         maxPitch={MAX_PITCH}
         maxZoom={MAX_ZOOM}
         minZoom={MIN_ZOOM}
@@ -51,7 +90,4 @@ const TerrainMap: FC = () => {
 };
 
 export default TerrainMap;
-
-
-
 
