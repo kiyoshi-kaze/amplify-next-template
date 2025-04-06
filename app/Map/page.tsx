79cc043a1b423aa0241540ff563c10d7f25d9fd8
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-04-06 21:59:17 +0900

    map

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -71,17 +71,47 @@ export default function App() {
 */
 
 "use client";
-import React, { useEffect } from 'react';
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format, parseISO } from "date-fns";
+
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
 export default function App() {
+
+  const [posts, setPosts] = useState<Array<{ Division: string; DivisionName: string; DivisionGeojson: string ;Controller?: string | null }>>([]);
+  console.log('listDivision（useState直後）=', posts);
+  
   useEffect(() => {
     listPost();
+    renderMap(); 
 
   }, []);
-  
+
   async function listPost() {
+    const { data, errors } = await client.queries.listDivision({
+      Controller: "Mutsu01",
+    });
+    console.log('listDivision（関数内）=', data);
+    if (data) {
+      setPosts(data as Array<{ Division: string; DivisionName: string; DivisionGeojson: string; Controller?: string | null }>); // 型を明示的にキャストする
+    }
+  }
+
+  async function renderMap() {
 
     const map = new maplibregl.Map({
       container: 'map',
