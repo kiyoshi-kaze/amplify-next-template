88e15113194738c1a9e2f41196d9dacb56e2b610
kiyoshi-kaze <kaze@kokontouzai.jp>
2025-04-08 22:47:12 +0900

    map

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -4,12 +4,66 @@
 
 
 "use client";
-import React, { useEffect } from 'react';
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format, parseISO } from "date-fns";
+
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
+import { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson';
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
 export default function App() {
+
+  const [divisionLists, setPosts] = useState<Array<{ Division: string; DivisionName: string; Geojson: string ;Controller?: string | null }>>([]);
+  console.log('divisionLists（State直後）=', divisionLists);
+
+  const Geojsons = divisionLists.map(divisionLists => divisionLists.Geojson);
+  console.log('divisionGeojsons（State直後）=', Geojsons[0]); 
+
   useEffect(() => {
+    async function fetchData() {
+        await listPost();
+    }
+    fetchData();
+  }, []);
+
+
+  //divisionLists の状態が更新された後に renderMap 関数を呼び出す
+  useEffect(() => {
+    if (divisionLists.length > 0) {
+      renderMap();
+    }
+  }, [divisionLists]);
+
+  async function listPost() {
+    const { data, errors } = await client.queries.listDivision({
+      Controller: "Mutsu01",
+    });
+    console.log('data（関数内）=', data);
+    //divisionLists の状態を更新
+    if (data) {
+      setPosts(data as Array<{ Division: string; DivisionName: string; Geojson: string; Controller?: string | null }>); // 型を明示的にキャストする
+    }
+  }
+
+  async function renderMap() {
+    console.log('Geojsons（renderMap内）=', Geojsons[0]);
+
+    const buildingData = Geojsons[0] ;
+
     const map = new maplibregl.Map({
       container: 'map',
       style: {
@@ -38,18 +92,21 @@ export default function App() {
           },
         ],
       },
-      center: [-87.61694, 41.86625],
+      center: [140.302994, 35.353503],
       zoom: 15.99,
       pitch: 40,
       bearing: 20,
     });
 
     map.on('load', () => {
+      //JSON.parseを使って文字列をGeoJSONオブジェクトに変換
+      const geojsonData = JSON.parse(buildingData);
+    
       map.addSource('floorplan', {
         type: 'geojson',
-        data: 'https://maplibre.org/maplibre-gl-js/docs/assets/indoor-3d-map.geojson',
+        data: geojsonData,
       });
-
+    
       map.addLayer({
         id: 'room-extrusion',
         type: 'fill-extrusion',
@@ -61,8 +118,12 @@ export default function App() {
           'fill-extrusion-opacity': 0.5,
         },
       });
+
     });
-  }, []);
+    
+
+
+  }
 
   return <div id="map" style={{ height: '100vh' }} />;
 }
@@ -86,31 +147,46 @@ import { format, parseISO } from "date-fns";
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
+import { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson';
+
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
 
-  const [divisionLists, setPosts] = useState<Array<{ Division: string; DivisionName: string; DivisionGeojson: string ;Controller?: string | null }>>([]);
+  const [divisionLists, setPosts] = useState<Array<{ Division: string; DivisionName: string; Geojson: string ;Controller?: string | null }>>([]);
+  console.log('divisionLists（State直後）=', divisionLists);
 
   useEffect(() => {
-    listPost();
-    renderMap(); 
-
+    async function fetchData() {
+        await listPost();
+    }
+    fetchData();
   }, []);
+
+
+  //divisionLists の状態が更新された後に renderMap 関数を呼び出す
+  useEffect(() => {
+    if (divisionLists.length > 0) {
+      renderMap();
+    }
+  }, [divisionLists]);
 
   async function listPost() {
     const { data, errors } = await client.queries.listDivision({
       Controller: "Mutsu01",
     });
-    console.log('divisionLists（関数内）=', data);
+    console.log('data（関数内）=', data);
+    //divisionLists の状態を更新
     if (data) {
-      setPosts(data as Array<{ Division: string; DivisionName: string; DivisionGeojson: string; Controller?: string | null }>); // 型を明示的にキャストする
+      setPosts(data as Array<{ Division: string; DivisionName: string; Geojson: string; Controller?: string | null }>); // 型を明示的にキャストする
     }
   }
 
   async function renderMap() {
+
+    //const buildingData = Geojsons[0] ;
 
     const map = new maplibregl.Map({
       container: 'map',
@@ -140,19 +216,22 @@ export default function App() {
           },
         ],
       },
-      center: [140.325250, 35.355518],
+      center: [140.302994, 35.353503],
       zoom: 15.99,
       pitch: 40,
       bearing: 20,
     });
 
     map.on('load', () => {
+      //JSON.parseを使って文字列をGeoJSONオブジェクトに変換
+      //const geojsonData = JSON.parse(buildingData);
+      const geojsonData = JSON.parse(divisionLists[0].Geojson);
+      console.log('geojsonData（renderMap内）=', geojsonData);
       map.addSource('floorplan', {
         type: 'geojson',
-        //data: 'https://maplibre.org/maplibre-gl-js/docs/assets/indoor-3d-map.geojson',
-        data: [divisionLists[0].DivisionGeojson],
+        data: geojsonData,
       });
-
+    
       map.addLayer({
         id: 'room-extrusion',
         type: 'fill-extrusion',
@@ -164,7 +243,10 @@ export default function App() {
           'fill-extrusion-opacity': 0.5,
         },
       });
+
     });
+    
+
 
   }
 
